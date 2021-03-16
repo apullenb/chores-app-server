98492f7957d3a03de5b5ae134f69c648c33aa5f4
Andrea Pullen <apullenb@gmail.com>
2021-03-16 17:02:23 -0500

    build server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 const app = require('./app');
-
-
-
-const PORT = process.env.PORT || 8000
+const { PORT } = require('./config');
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Server listening at http://localhost:${PORT}`);
 })
